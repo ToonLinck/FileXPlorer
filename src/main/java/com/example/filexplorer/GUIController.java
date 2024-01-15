6df84dffe83e1fd6f0143e34ce09a3d7b8ca5a50
ToonLinck <Johanlinck96@gmail.com>
2024-01-15 11:08:16 +0100

    Updated GUI

--- src/main/java/com/example/filexplorer/GUIController.java
+++ src/main/java/com/example/filexplorer/GUIController.java
@@ -1,5 +1,6 @@
 package com.example.filexplorer;
 
+import FileXPlorer.Backend.DateiInfo;
 import javafx.*;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
@@ -15,34 +16,33 @@ public class GUIController {
     public List<DateiInfo> dateiList = new ArrayList<DateiInfo>();
 
     @FXML
-    ChoiceBox MainWindowToolBarChoiceBox;
+    ChoiceBox mainWindowToolBarChoiceBox;
 
     @FXML
-    Button MainWindowToolBarZurueckButton, MainWindowToolBarOeffnenButton, MainWindowToolBarLoeschenButton,
-            MainWindowToolBarNeuButton, MainWindowToolSubBarAnzeigenButton, MainWindowToolSubBarBearbeitenbButton,
-            MainWindowToolSubBarSuchenButton;
+    Button mainWindowToolBarZurueckButton, mainWindowToolBarOeffnenButton, mainWindowToolBarLoeschenButton,
+            mainWindowToolBarNeuButton, mainWindowToolSubBarAnzeigenButton, mainWindowToolSubBarBearbeitenbButton,
+            mainWindowToolSubBarSuchenButton;
 
     @FXML
-    CheckBox MainWindowToolSubBarOrdnerAnzeigenCheckBox, MainWindowToolSubBarDateienAnzeigenCheckBox1;
+    CheckBox mainWindowToolSubBarOrdnerAnzeigenCheckBox, mainWindowToolSubBarDateienAnzeigenCheckBox1;
 
     @FXML
     Accordion mainWindowAccordion;
 
     @FXML
     public void initialize() {
-        MainWindowToolBarChoiceBox.getItems().add("Groeße ASC");
-        MainWindowToolBarChoiceBox.getItems().add("Groeße DESC");
-        MainWindowToolBarChoiceBox.getItems().add("ALter ASC");
-        MainWindowToolBarChoiceBox.getItems().add("Alter DESC");
-        MainWindowToolBarChoiceBox.getItems().add("Alphabetisch");
+        mainWindowToolBarChoiceBox.getItems().add("Groeße ASC");
+        mainWindowToolBarChoiceBox.getItems().add("Groeße DESC");
+        mainWindowToolBarChoiceBox.getItems().add("ALter ASC");
+        mainWindowToolBarChoiceBox.getItems().add("Alter DESC");
+        mainWindowToolBarChoiceBox.getItems().add("Alphabetisch");
 
         DateiInfo testDatei = new DateiInfo();
         testDatei.dateiName = "test.txt";
         testDatei.dateiTyp = "Textdatei";
         testDatei.dateigröße = 2333;
+        testDatei.trueDateiPfad = "C:/test.txt";
 
-        Date aenderung = new Date();
-        aenderung.setTime(1);
 
         dateiList.add(testDatei);
 
@@ -53,14 +53,22 @@ public class GUIController {
 
 
     /**
-     * Diese Methode wird ausgeführt, wenn der "Open" knopf gedrückt wird
+     * Diese Methode wird ausgeführt, wenn der "Open" knopf gedrückt wird. Wenn der beigelegte Dateipfad zu einem
+     * Ordner führt, wird dieser Geöffnet und Inhalte werden geladen.
      */
-    public void open() {
+    public void openFolder(String dateiPfad) {
+        if (dateiPfad != null && !dateiPfad.isEmpty()){
 
+        }
     }
 
-
+    /**
+     * Läd die Dateien im Ausgewählten Ordner und zeigt sie in der Oberfläche an
+     */
     public void loadFileOverview() {
+
+        mainWindowAccordion.getPanes().clear();
+
         for (DateiInfo dateiInfo : dateiList)
         {
             mainWindowAccordion.getPanes().add(createElementFromDateiInfo(dateiInfo));
@@ -75,8 +83,7 @@ public class GUIController {
      */
     private TitledPane createElementFromDateiInfo (DateiInfo dateiInfo) {
         TitledPane returnTitledPane = new TitledPane();
-        returnTitledPane.setText(dateiInfo.getDateiName());
-        returnTitledPane.setText(dateiInfo.dateiName);
+        returnTitledPane.setText(dateiInfo.trueDateiPfad);
         returnTitledPane.setAnimated(false);
 
             AnchorPane nAnchorPane = new AnchorPane();
@@ -101,15 +108,15 @@ public class GUIController {
                     groesseLabel.setLayoutY(84);
 
                 TextField nameTextField = new TextField();
-                    nameTextField.setText(dateiInfo.getDateiName());
+                    nameTextField.setText(dateiInfo.dateiName);
                     nameTextField.setPrefSize(122,25);
                     nameTextField.setLayoutX(83);
                     nameTextField.setLayoutY(14);
 
                 Button saveButton = new Button();
                     saveButton.setText("Änderungen speichern");
-                    saveButton.setLayoutY(310);
-                    saveButton.setLayoutX(488);
+                    saveButton.setLayoutY(200);
+                    saveButton.setLayoutX(450);
                     saveButton.setOnAction(event -> {
                          if (!nameTextField.getText().isEmpty()) {
                              dateiInfo.dateiName = nameTextField.getText();
@@ -155,6 +162,25 @@ public class GUIController {
 
     private void ChangeDateiInfo(DateiInfo nDateiInfo) {
         //todo Datei info wird geändert
+    }
+
+    /**
+     * Sucht in der dateiList Liste nach einem DateiInfo Objekt, wobei der DateiPfad gegeben wird
+     *
+     * @param searchTruePath der Pfad der Datei, nach dessen DateiInfo Objekt gesucht werden soll. Ausgabe defaultet auf Null, falls nicht vorhanden
+     * @return Das zugehörige DateiInfo Objekt aus der Liste dateiList. Defaultet auf Null als ausgabe, wenn nicht vorhanden.
+     */
+    private DateiInfo findDateiInfoFromTruePath (String searchTruePath) {
+        if (searchTruePath == null || searchTruePath.isEmpty())
+            return null;
+
+        for (DateiInfo dateiInfo: dateiList) {
+            if (dateiInfo.trueDateiPfad.equals(searchTruePath)) {
+                return dateiInfo;
+            }
+        }
+
+        return null;
     }
 
 }
